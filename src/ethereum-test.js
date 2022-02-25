2ba37869edf65cad7f3e17f008ced6a2b68f5655
Jon <Jon.Wilkinson@rockets.utoledo.edu>
2022-02-25 13:47:34 -0500

    Added ethereum test file

---
+++ src/ethereum-test.js
@@ -0,0 +1,8 @@
+var Web3 = require("web3")
+const web3 = new Web3("https://cloudflare-eth.com")
+
+function getBlockNumber() {
+    return web3.eth.getBlockNumber();
+}
+
+export default getBlockNumber;
