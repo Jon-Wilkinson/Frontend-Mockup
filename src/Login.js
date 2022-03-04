45dd93ba7b5f120a454021c098093a3954f803b6
Jon <Jon.Wilkinson@rockets.utoledo.edu>
2022-03-04 11:33:22 -0500

    Login page

--- src/Login.js
+++ src/Login.js
@@ -2,54 +2,33 @@ import React from 'react';
 import './css/Login.css';
 import { useNavigate } from 'react-router-dom'
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-        };
+function Login() {
+    let navigate = useNavigate();
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({
-            username: event.target.username,
-            password: event.target.password,
-        });
-    }
-
-    handleSubmit(event) {
-        let navigate = useNavigate();
-        navigate('mainpage');
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <header class="Login-header">
-                <h1>
-                    This is the login page!
-                </h1>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Username: <br></br>
-                        <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
-                    </label>
-                    <br></br>
-                    <label>
-                        Password: <br></br>
-                        <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-                    </label>
-                    <br></br>
-                    <input type="submit" value="Submit" />
-                </form>
-            </header>
-        );
-    }
+    return (
+        <header className="Login-header">
+            <h1>
+                This is the login page!
+            </h1>
+            <form>
+                <label>
+                    Username: <br></br>
+                    <input type="text" name="username"/>
+                </label>
+                <br></br>
+                <label>
+                    Password: <br></br>
+                    <input type="password" name="password"/>
+                </label>
+                <br></br>
+                <input type="submit" value="Submit" onClick={(event) => {
+                  console.log('sumbitted');
+                  navigate('/mainpage');
+                  event.preventDefault();
+                }}/>
+            </form>
+        </header>
+    );
 }
-
 
 export default Login;
